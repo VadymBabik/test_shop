45ed1fc4522e018d58dfd1fbdbbb41bb8f9e040b
vadymbabik <vadym.babik@gmail.com>
2021-10-25 01:40:09 +0300

    add server listen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello!");
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
+
+const app = express();
+config();
+const PORT = process.env.APP_PORT || 5000;
+app.get("/", async (req: Request, res: Response) => {
+  console.log(req.url);
+  res.status(200).send("Ok!");
+});
+
+app.listen(PORT, () => console.log(`App lisen to ${PORT}...`));
